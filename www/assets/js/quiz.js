2d02b8fae3f34793a30ab09e337f1043b8007b75
Lea <lea.chen@edu.esiee-it.fr>
2025-04-13 20:27:32 +0200

    Add quiz pages

---
+++ www/assets/js/quiz.js
@@ -0,0 +1,11 @@
+const questions =[
+    {
+        question: "Which space telescope discovered thousands of exoplanets by observing transits?",
+        answer: [
+            {text: "Gaia (ESA)", correct: false},
+            {text: "Kepler (NASA)", correct: true},
+            {text: "TESS (NASA)", correct: false},
+            {text: "CHEOPS (ESA))", correct: false},
+        ]
+    }
+]
